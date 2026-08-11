1c5f83f0834fa2ca49d8e98c22cf5400a5291ca4
lonshas <estevaoguaitolini@gmail.com>
2026-08-11 11:37:46 -0300

    Função de cadastro de livros e de lista concluídas

--- main.py
+++ main.py
@@ -4,28 +4,64 @@ os.system("cls")
 
 estrutura_de_livro = ['titulo','autor','ano de publicacao','código','status']
 
+def cadastro_de_livro():
+    titulo = input("Digite o título do livro: ")
+    autor = input("Digite o autor do livro: ")
+    ano = input("Digite o ano de publicacao do livro: ")
+    codigo = input("Digite o codigo(ISBN) do livro: ")
+    status = "Disponivel"
+    
+    arquivo_existe = os.path.exists('livros.csv')
+
+    with open('livros.csv','a',newline= '') as cadastro:
+
+        livro = {'titulo': titulo,
+                'autor': autor,
+                'ano de publicacao': ano,
+                'código': codigo,
+                'status': status
+                }
+        
+        writer = csv.DictWriter(cadastro,fieldnames=estrutura_de_livro)
+
+        if not arquivo_existe:
+            writer.writeheader()
+        writer.writerow(livro)
+    print("\nLivro cadastrado!")
+
+def listagem_de_livros():
+    if not os.path.exists('livros.csv'):
+        print("Nenhum livro cadastrado.")
+        return
+    with open('livros.csv', 'r', newline='') as listagem:
+        leitor = csv.DictReader(listagem)
+    for livro in leitor:
+        print(f"Título: {livro['titulo']}")
+        print(f"Autor: {livro['autor']}")
+        print(f"Ano de publicacao: {livro['ano']}")
+        print(f"Codigo: {livro['codigo']}")
+        print(f"Título: {livro['status']}")
+        print("-------------------------------------------------")
+
+
+    
+
 while True:
     print("----------Gerenciador da Biblioteca----------\n--------------------MENU---------------------\n")
     escolha_menu = input("1- CADASTRAR LIVRO\n2- EMPRÉSTIMO DE LIVRO\n3- DEVOLUÇÃO DE LIVRO\n4- LISTAR LIVROS\n5- BUSCAR LIVRO\n6- ORDENAR LISTA\n7- SAIR\nEscreva o comando aqui: ")
     if escolha_menu == "1":
         cadastro_de_livro()
     elif escolha_menu =="2":
-        pass
+        emprestimo_de_livro()
     elif escolha_menu =="3":
         pass
     elif escolha_menu =="4":
-        pass
+        listagem_de_livros()
     elif escolha_menu =="5":
         pass
     elif escolha_menu =="6":
         pass
     elif escolha_menu =="7":   
         break
-
-
-
-def cadastro_de_livro():
-    with open('livros.csv','a',newline= '') as cadastro:
-        livro = {'titulo':titulo,'autor':autor,'ano de publicacao':ano,'código':isbn,'status':status}
-        writer = csv.DictWriter('livros.csv',fieldnames=estrutura_de_livro)
-        writer.writerow(livro)
+    else:
+        print("Comando inválido!")
